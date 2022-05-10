92efffbc3e2d4519c1c2d8914daa853e32ded0bf
abhilashpq <103559965+abhilashpq@users.noreply.github.com>
2022-05-10 23:16:11 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class EvenOdd
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the m");
+int m=sc.nextInt();
+System.out.println("enter the n");
+int n=sc.nextInt();
+{
+if(m>n)
+System.out.println(m+"is largest");
+else
+System.out.println(n+"is largest");
+}
+}
+}
